44451326c082e9a845080bc43e50b65031b4a0f6
Vepa Babayev <vepababayev03@gmail.com>
2025-04-23 11:15:55 +0200

    navbar yasadym

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,37 +1,46 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("data"),
         backgroundColor: Colors.amber,
       ),
       drawer: Drawer(
-        child: ListView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ListTile(
-              leading: Icon(Icons.abc),
-              title: Text("data"),
-            )
+            Text("data"),
+            Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text("Paylas"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text("Cykalga"),
+                  onTap:(){
+                    Navigator.pop(context);
+                  }
+                ),
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text("Biz Hakynda"),
+                )
+              ],
+            ),
           ],
         ),
       ),
       body: Center(
-        child: Builder(builder: (context){
-          return Column(
-            children: [
-              Text("data"),
-              SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){
-                print("object");
-              }, child: Text("button"))
-            ],
-          );
-        }),
+        
       ),
     );
   }
