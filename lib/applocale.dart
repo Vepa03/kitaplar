988919584a2f4f5b6d9bedac116ada41c0f6fa70
Vepa Babayev <vepababayev03@gmail.com>
2025-04-28 22:20:17 +0200

    pozdy

--- lib/applocale.dart
+++
@@ -1,6 +0,0 @@
-mixin Applocale {
-  static const String title = "title";
-
-  static const Map<String, dynamic> EN = {title: "Smart"};
-  static const Map<String, dynamic> TR = {title: "Akylly"};
-}
