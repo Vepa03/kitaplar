21559652c4d2b82b57aa9c0986ab0634bbe59da6
Vepa Babayev <vepababayev03@gmail.com>
2025-05-03 23:56:27 +0200

    surat goshuldy

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:kitaplar/applocale.dart';
 
-class AboutUs extends StatelessWidget {
+class AboutUs extends StatefulWidget {
   const AboutUs({super.key});
+  
+  @override
+  State<AboutUs> createState() => _AboutUsState();
+}
 
+class _AboutUsState extends State<AboutUs> {
+ 
+  
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         title: Text(Applocale.about_us.getString(context), style: Theme.of(context).textTheme.titleLarge,),
         elevation: 2,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              child: Image.asset("lib/assets/images/books.png", width: 150, height: 150,),
+            ),
+            Text(Applocale.title.getString(context), style: Theme.of(context).textTheme.titleLarge,)
+          ],
+        ),
       ),
     );
   }
