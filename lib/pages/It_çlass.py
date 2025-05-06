13b265a169c127c6354e43654cf07565f134beba
Vepa Babayev <vepababayev03@gmail.com>
2025-05-07 00:47:15 +0200

    about us onarylydy

--- lib/pages/It_çlass.py
+++ lib/pages/It_çlass.py
@@ -1,46 +1,64 @@
-# A = [10,4,6,8]
+A = [10,4,6,8]
 
-# a = A[0]
-# b = A[0]
+a = A[0]
+b = A[0]
 
-# for i in range (len(A)):
-#     if A[i]>a:
-#         a=A[i]
-#     if A[i]<b:
-#         b=A[i]
+for i in range (len(A)):
+    if A[i]>a:
+        a=A[i]
+    if A[i]<b:
+        b=A[i]
 
-# print(a-b)
-
-
-
-
-# A = [12,13,4,6,8,9]
-# B = []
-
-# for i in range (len(A)):
-#     if A[i]%3==0:
-#         B.append(A[i])
-
-# print(B)
+print(a-b)
 
 
 
 
-# odd_number = []
-# even_number = []
-# all_number = []
-# while True:
-#     x = int(input(""))
-#     if x == 0:
-#         break
-#     if x%2==0:
-#         even_number.append(x)
-#     else:
-#         odd_number.append(x)
-#     all_number.append(x)
-# print(even_number, "Even numbers")
-# print(odd_number, "Odd numbers")
-# print(all_number, "all number")
+
+
+
+
+A = [12,13,4,6,8,9]
+B = []
+
+for i in range (len(A)):
+    if A[i]%3==0:
+        B.append(A[i])
+
+print(B)
+
+
+
+
+
+
+
+
+
+odd_number = []
+even_number = []
+all_number = []
+while True:
+    x = int(input(""))
+    if x == 0:
+        break
+    if x%2==0:
+        even_number.append(x)
+    else:
+        odd_number.append(x)
+    all_number.append(x)
+print(even_number, "Even numbers")
+print(odd_number, "Odd numbers")
+print(all_number, "all number")
+
+
+
+
+
+
+
+
+
 
 
 odd_number = []
@@ -65,3 +83,4 @@ print(odd_number, "Odd numbers")
 print(all_number, "all number")
 print(neg_number, "Negative number")
 
+ 
--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -3,51 +3,47 @@ import 'package:flutter_localization/flutter_localization.dart';
 import 'package:kitaplar/applocale.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class AboutUs extends StatefulWidget {
+class AboutUs extends StatelessWidget {
   const AboutUs({super.key});
-  
-  @override
-  State<AboutUs> createState() => _AboutUsState();
-}
 
-class _AboutUsState extends State<AboutUs> {
-  final Map<String, String> socialLinks = {
-  "Instagram": "https://www.instagram.com/vepa03_/",
-  "GitHub": "https://github.com/your_username",
-  "Twitter": "https://twitter.com/your_username",
-  "WebSite": "https://babayev.vercel.app/",
+  static  Map<String, Uri> socialLinks = {
+    "Instagram": Uri.parse("https://www.instagram.com/vepa03_/"),
+    "GitHub": Uri.parse("https://github.com/Vepa03"),
+    "Twitter": Uri.parse("https://x.com/vepaskaa"),
+    "WebSite": Uri.parse("https://babayev.vercel.app/"),
+  };
 
-};
-void _launchURL(String urlString) async {
-    final url = Uri.parse(urlString);
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
+  Future<void> _launchURL(BuildContext context, Uri url) async {
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Could not launch $urlString")),
+        SnackBar(content: Text("Could not launch $url")),
       );
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    double width_size = MediaQuery.of(context).size.width;
-    double height_size = MediaQuery.of(context).size.height;
-    return  Scaffold(
+    double widthSize = MediaQuery.of(context).size.width;
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text(Applocale.about_us.getString(context), style: Theme.of(context).textTheme.titleLarge,),
+        title: Text(
+          Applocale.about_us.getString(context),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
         elevation: 2,
         actions: [
           PopupMenuButton<String>(
             onSelected: (value) {
-              _launchURL(socialLinks[value]!);
+              _launchURL(context, socialLinks[value]!);
             },
             itemBuilder: (context) => [
               PopupMenuItem(
                 value: "Instagram",
                 child: Row(
                   children: [
-                    Image.asset("lib/assets/images/instagram.png", width: width_size*0.05,),
-                    SizedBox(width: width_size*0.05,),
+                    Image.asset("lib/assets/images/instagram.png", width: widthSize * 0.05),
+                    SizedBox(width: widthSize * 0.05),
                     Text("Instagram"),
                   ],
                 ),
@@ -56,8 +52,8 @@ void _launchURL(String urlString) async {
                 value: "GitHub",
                 child: Row(
                   children: [
-                    Image.asset("lib/assets/images/github.png", width: width_size*0.05,),
-                    SizedBox(width: width_size*0.05,),
+                    Image.asset("lib/assets/images/github.png", width: widthSize * 0.05),
+                    SizedBox(width: widthSize * 0.05),
                     Text("GitHub"),
                   ],
                 ),
@@ -66,8 +62,8 @@ void _launchURL(String urlString) async {
                 value: "Twitter",
                 child: Row(
                   children: [
-                    Image.asset("lib/assets/images/twitter.png", width: width_size*0.05,),
-                    SizedBox(width: width_size*0.05,),
+                    Image.asset("lib/assets/images/twitter.png", width: widthSize * 0.05),
+                    SizedBox(width: widthSize * 0.05),
                     Text("Twitter"),
                   ],
                 ),
@@ -76,8 +72,8 @@ void _launchURL(String urlString) async {
                 value: "WebSite",
                 child: Row(
                   children: [
-                    Image.asset("lib/assets/images/world-wide-web.png", width: width_size*0.05,),
-                    SizedBox(width: width_size*0.05,),
+                    Image.asset("lib/assets/images/world-wide-web.png", width: widthSize * 0.05),
+                    SizedBox(width: widthSize * 0.05),
                     Text("WebSite"),
                   ],
                 ),
@@ -90,13 +86,22 @@ void _launchURL(String urlString) async {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Card(
                 elevation: 2,
-                child: Image.asset("lib/assets/images/books.png", width: 150, height: 150,),
+                child: Image.asset("lib/assets/images/books.png", width: 150, height: 150),
               ),
-              Text(Applocale.title.getString(context), style: Theme.of(context).textTheme.titleLarge,),
-              Text(Applocale.biz_hakynda.getString(context), style: Theme.of(context).textTheme.titleSmall,)
+              const SizedBox(height: 10),
+              Text(
+                Applocale.title.getString(context),
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                Applocale.biz_hakynda.getString(context),
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
             ],
           ),
         ),
