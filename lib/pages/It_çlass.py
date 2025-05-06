743f826c88d098c52f02e25d731d25954400973f
Vepa Babayev <vepababayev03@gmail.com>
2025-05-06 10:35:11 +0200

    aboout us onaryldy

---
+++ lib/pages/It_çlass.py
@@ -0,0 +1,67 @@
+# A = [10,4,6,8]
+
+# a = A[0]
+# b = A[0]
+
+# for i in range (len(A)):
+#     if A[i]>a:
+#         a=A[i]
+#     if A[i]<b:
+#         b=A[i]
+
+# print(a-b)
+
+
+
+
+# A = [12,13,4,6,8,9]
+# B = []
+
+# for i in range (len(A)):
+#     if A[i]%3==0:
+#         B.append(A[i])
+
+# print(B)
+
+
+
+
+# odd_number = []
+# even_number = []
+# all_number = []
+# while True:
+#     x = int(input(""))
+#     if x == 0:
+#         break
+#     if x%2==0:
+#         even_number.append(x)
+#     else:
+#         odd_number.append(x)
+#     all_number.append(x)
+# print(even_number, "Even numbers")
+# print(odd_number, "Odd numbers")
+# print(all_number, "all number")
+
+
+odd_number = []
+even_number = []
+all_number = []
+neg_number = []
+while True:
+    x = int(input(""))
+    if x == 0:
+        break
+    if x%2==0:
+        even_number.append(x)
+    elif x< 0:
+        neg_number.append(x)
+    else:
+        odd_number.append(x)
+    
+    all_number.append(x)
+
+print(even_number, "Even numbers")
+print(odd_number, "Odd numbers")
+print(all_number, "all number")
+print(neg_number, "Negative number")
+
--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -11,47 +11,76 @@ class AboutUs extends StatefulWidget {
 }
 
 class _AboutUsState extends State<AboutUs> {
-  void _launchURL(String urlString) async {
+  final Map<String, String> socialLinks = {
+  "Instagram": "https://www.instagram.com/vepa03_/",
+  "GitHub": "https://github.com/your_username",
+  "Twitter": "https://twitter.com/your_username",
+  "WebSite": "https://babayev.vercel.app/",
+
+};
+void _launchURL(String urlString) async {
     final url = Uri.parse(urlString);
     if (await canLaunchUrl(url)) {
       await launchUrl(url);
     } else {
-      throw "Could not launch $url";
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Could not launch $urlString")),
+      );
     }
   }
   @override
   Widget build(BuildContext context) {
+    double width_size = MediaQuery.of(context).size.width;
+    double height_size = MediaQuery.of(context).size.height;
     return  Scaffold(
       appBar: AppBar(
         title: Text(Applocale.about_us.getString(context), style: Theme.of(context).textTheme.titleLarge,),
         elevation: 2,
         actions: [
           PopupMenuButton<String>(
-            onSelected: (value){
-              switch (value) {
-                case "Instagram":
-                  _launchURL("https://www.instagram.com/vepa03_/");
-                  break;
-                case "GitHub":
-                  _launchURL("https://github.com/your_username");
-                  break;
-                case "Twitter":
-                  _launchURL("https://twitter.com/your_username");
-                  break;
-              }
+            onSelected: (value) {
+              _launchURL(socialLinks[value]!);
             },
             itemBuilder: (context) => [
               PopupMenuItem(
                 value: "Instagram",
-                child: Text("Instagram"),
+                child: Row(
+                  children: [
+                    Image.asset("lib/assets/images/instagram.png", width: width_size*0.05,),
+                    SizedBox(width: width_size*0.05,),
+                    Text("Instagram"),
+                  ],
+                ),
               ),
               PopupMenuItem(
                 value: "GitHub",
-                child: Text("GitHub"),
+                child: Row(
+                  children: [
+                    Image.asset("lib/assets/images/github.png", width: width_size*0.05,),
+                    SizedBox(width: width_size*0.05,),
+                    Text("GitHub"),
+                  ],
+                ),
               ),
               PopupMenuItem(
                 value: "Twitter",
-                child: Text("Twitter"),
+                child: Row(
+                  children: [
+                    Image.asset("lib/assets/images/twitter.png", width: width_size*0.05,),
+                    SizedBox(width: width_size*0.05,),
+                    Text("Twitter"),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                value: "WebSite",
+                child: Row(
+                  children: [
+                    Image.asset("lib/assets/images/world-wide-web.png", width: width_size*0.05,),
+                    SizedBox(width: width_size*0.05,),
+                    Text("WebSite"),
+                  ],
+                ),
               ),
             ],
           )
